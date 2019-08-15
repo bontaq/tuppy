10303c81e46e50e3068c88a280b762265af09920
Ian Davidson <bontaq@gmail.com>
2019-08-15 00:28:34 -0400

    Printer is working for let

--- src/Language.hs
+++ src/Language.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Language where
 
 import Data.List (intersperse)
@@ -70,14 +72,16 @@ preludeDefs
     , ("twice"
       , ["f"]
       , EAp
-        (EAp (EVar "compose") (EVar "f")) (EVar "f")) ]
-
-newline :: [Char]
-newline = "\n"
+        (EAp (EVar "compose") (EVar "f")) (EVar "f"))
+    , ("lettest"
+      , ["f"]
+      , ELet False
+        [("f", (EAp (EVar "g") (EVar "x")))]
+        (EAp (EVar "test") (EVar "f"))) ]
 
 prettyPrintDefn :: (Name, CoreExpr) -> Iseq
 prettyPrintDefn (name, expr) =
-  iConcat [ iStr name, iStr " = ", prettyPrintExpr expr ]
+  iConcat [ iStr name, iStr " = ", iIndent $ prettyPrintExpr expr ]
 
 prettyPrintDefns :: [(Name, CoreExpr)] -> Iseq
 prettyPrintDefns defns =
@@ -87,8 +91,6 @@ prettyPrintDefns defns =
 
 prettyPrintExpr :: CoreExpr -> Iseq
 
--- prettyPrintExpr (ENum n) = IStr n
-
 prettyPrintExpr (EVar v) = IStr v
 
 prettyPrintExpr (EAp e1 e2) =
@@ -97,8 +99,8 @@ prettyPrintExpr (EAp e1 e2) =
   `iAppend` prettyPrintExpr e2
 
 prettyPrintExpr (ELet isrec defns expr)
-  = iConcat [ iStr keyword, iNewline
-            , iStr " ", iIndent (prettyPrintDefns defns), iNewline
+  = iConcat [ iStr keyword, iIndent iNewline
+            , (prettyPrintDefns defns)
             , iStr "in ", prettyPrintExpr expr]
   where
     keyword | not isrec = "let"
@@ -118,46 +120,75 @@ prettyPrintAExpr e
 
 prettyPrintSC :: ScDefn Name -> Iseq
 prettyPrintSC (name, args, exp) =
-  iConcat [ iStr name
-          , iInterleave (iStr " " ) $ map iStr args, iNewline
-          , prettyPrintExpr exp, iNewline ]
+  iConcat [ iStr name, iStr " "
+          , iInterleave (iStr " " ) $ map iStr args, iStr "= "
+          , prettyPrintExpr exp ]
 
-prettyPrintProgram :: CoreProgram -> Iseq
-prettyPrintProgram scs = iConcat $ map prettyPrintSC scs
+prettyPrintProgram :: [CoreScDefn] -> Iseq
+prettyPrintProgram scs =
+  iInterleave sep (map prettyPrintSC scs)
+  where
+    sep = iConcat [iStr ";", iNewline]
 
 data Iseq = INil
           | IStr String
           | IAppend Iseq Iseq
+          | IIndent Iseq
+          | INewline
 
 iNil :: Iseq
 iNil = INil
 
 iStr :: String -> Iseq
-iStr = IStr
+iStr str  = IStr str
 
 iAppend :: Iseq -> Iseq -> Iseq
 iAppend seq1 seq2 = IAppend seq1 seq2
 
 iIndent :: Iseq -> Iseq
-iIndent seq = seq
+iIndent seq = IIndent seq
 
 iNewline :: Iseq
-iNewline = IStr "\n"
+iNewline = INewline
 
 iConcat :: [Iseq] -> Iseq
 iConcat = foldr iAppend iNil
 
 iInterleave :: Iseq -> [Iseq] -> Iseq
-iInterleave spacer = foldr (iAppend . iAppend spacer) iNil
+iInterleave _ [] = iNil
+iInterleave ins (i:is) = (i `iAppend` ins) `iAppend` iInterleave ins is
 
-flatten :: [Iseq] -> String
-flatten [] = ""
-flatten (INil : seqs) = flatten seqs
-flatten (IStr s : seqs) = s ++ (flatten seqs)
-flatten (IAppend s1 s2 : ss) = flatten (s1 : s2 : ss)
+iNum :: Int -> Iseq
+iNum n = iStr (show n)
+
+iFWNum :: Int -> Int -> Iseq
+iFWNum width n =
+  iStr (space (width - length digits) ++ digits)
+  where
+    digits = show n
+
+iLayn :: [Iseq] -> Iseq
+iLayn seqs = iConcat (map layItem (zip [1..] seqs))
+  where
+    layItem (n, seq)
+      = iConcat [ iFWNum 4 n, iStr ") ", iIndent seq, iNewline ]
+
+space :: Int -> String
+space n = take n $ repeat ' '
+
+flatten :: Int -> [(Iseq, Int)] -> String
+flatten _ [] = ""
+flatten col ((INewline, indent) : seqs) =
+  '\n' : (space indent) ++ (flatten indent seqs)
+flatten col ((IIndent seq, _) : seqs) =
+  flatten col ((seq, col + 2) : seqs)
+flatten col ((IAppend s1 s2, indent) : ss) =
+  flatten col ((s1, indent) : (s2, indent) : ss)
+flatten col ((INil, _) : seqs) = flatten col seqs
+flatten col ((IStr s, _) : seqs) = s ++ (flatten col seqs)
 
 iDisplay :: Iseq -> String
-iDisplay seq = flatten [seq]
+iDisplay seq = flatten 0 [(seq, 0)]
 
 mkMultiAp :: Int -> CoreExpr -> CoreExpr -> CoreExpr
 mkMultiAp n e1 e2 = foldl EAp e1 (take n e2s)
