560985c573c4a4cfbda1b84312a9ad83124fe64c
Ian Davidson <bontaq@gmail.com>
2019-11-02 21:38:28 -0400

    Start on a command line interface to run it

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,33 @@
 module Main where
 
 import Lib
+import Options.Applicative
+
+data Args = Args
+            { compile :: Bool
+            , file :: String
+            }
+
+handler :: Parser Args
+handler = Args
+  <$> switch
+  ( long "compile"
+    <> short 'c'
+    <> help "" )
+  <*> strOption
+  ( long "file"
+    <> short 'f'
+    <> help "The file to compile"
+    <> metavar "STRING"
+  )
 
 main :: IO ()
-main = print "hello"
+main = run =<< execParser opts
+  where
+    opts = info (handler <**> helper)
+      ( fullDesc
+        <> progDesc "Compile a program"
+        <> header "hello" )
+
+run :: Args -> IO ()
+run (Args compile file) = putStrLn $ (show compile) <> " " <> (show file)
