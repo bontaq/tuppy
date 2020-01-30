e75c6a028bb1d3a4f614dcd0479d4b71c714d368
Ian Davidson <bontaq@gmail.com>
2020-01-29 20:41:32 -0500

    Finish with examples

--- src/NewNewTypeChecker.hs
+++ src/NewNewTypeChecker.hs
@@ -1,5 +1,7 @@
 module NewNewTypeChecker where
 
+import Control.Monad.Except
+
 --
 -- https://www.andres-loeh.de/LambdaPi/LambdaPi.pdf
 -- A tutorial implementation of a dependently typed lambda calculus
@@ -92,3 +94,104 @@ type Context = [(Name, Info)]
 --
 
 type Result a = Either String a
+
+kindCheckable :: Context -> Type -> Kind -> Result ()
+kindCheckable context (TFree x) Star =
+  case lookup x context of
+    Just (HasKind Star) -> return ()
+    Nothing -> throwError "unknown identifier"
+
+kindCheckable context (Fun k k') Star =
+  do
+    kindCheckable context k Star
+    kindCheckable context k' Star
+
+typeInfer0 :: Context -> InferableTerm -> Result Type
+typeInfer0 context term = typeInfer 0 context term
+
+typeInfer :: Int -> Context -> InferableTerm -> Result Type
+typeInfer i context (Ann checkableTerm type') =
+  do
+    kindCheckable context type' Star
+    typeCheckable i context checkableTerm type'
+    return type'
+
+typeInfer i context (Free x) =
+  case lookup x context of
+    Just (HasType t) -> return t
+    Nothing -> throwError "unknown identifier"
+
+typeInfer i context (e :@: e') =
+  do
+    omega <- typeInfer i context e
+    case omega of
+      Fun t t' -> do
+        typeCheckable i context e' t
+        return t'
+      _ -> throwError "illegal application"
+
+typeCheckable :: Int -> Context -> CheckableTerm -> Type -> Result ()
+typeCheckable i context (Inf e) t =
+  do
+    t' <- typeInfer i context e
+    unless (t == t') (throwError "type mismatch")
+typeCheckable i context (Lam e) (Fun t t') =
+  typeCheckable (i + 1) ((Local i, HasType t) : context)
+                (substCheck 0 (Free (Local i)) e) t'
+typeCheckable i context _ _ =
+  throwError "type mistmatch"
+
+substInfer :: Int -> InferableTerm -> InferableTerm -> InferableTerm
+substInfer i r (Ann e t)  = Ann (substCheck i r e) t
+substInfer i r (Bound j)  = if i == j then r else Bound j
+substInfer i r (Free y)   = Free y
+substInfer i r (e :@: e') = substInfer i r e :@: substCheck i r e'
+
+substCheck :: Int -> InferableTerm -> CheckableTerm -> CheckableTerm
+substCheck i r (Inf e) = Inf (substInfer i r e)
+substCheck i r (Lam e) = Lam (substCheck (i + 1) r e)
+
+--
+-- Quoting (to print Funs)
+--
+
+quote0 :: Value -> CheckableTerm
+quote0 = quote 0
+
+quote :: Int -> Value -> CheckableTerm
+quote i (VLam f)     = Lam (quote (i + 1) (f (vfree (Quote i))))
+quote i (VNeutral n) = Inf (neutralQuote i n)
+
+neutralQuote :: Int -> Neutral -> InferableTerm
+neutralQuote i (NFree x)  = boundfree i x
+neutralQuote i (NApp n v) = neutralQuote i n :@: quote i v
+
+boundfree :: Int -> Name -> InferableTerm
+boundfree i (Quote k) = Bound (i - k - 1)
+boundfree i x         = Free x
+
+--
+-- Examples
+--
+
+id' = Lam (Inf (Bound 0))
+const' = Lam (Lam (Inf (Bound 1)))
+tfree a = TFree (Global a)
+free x = Inf (Free (Global x))
+term1=Ann id' (Fun (tfree "a") (tfree "a")) :@: free "y"
+term2=Ann const' (Fun
+                  (Fun (tfree "b") (tfree "b"))
+                  (Fun (tfree "a") (Fun (tfree "b") (tfree "b"))))
+  :@: id' :@: free "y"
+env1 = [ (Global "y", HasType (tfree "a"))
+       , (Global "a", HasKind Star) ]
+env2 = [ (Global "b", HasKind Star) ] <> env1
+
+-- quote0 (evalInferable term1 [])
+-- > Inf (Free (Global "y"))
+-- quote0 (evalInferable term2 [])
+-- > Lam (Inf (Bound 0))
+-- typeInfer0 env1 term1
+-- > Right (TFree (Global "a"))
+-- typeInfer0 env2 term2
+-- > Right (Fun (TFree (Global "b")) (TFree (Global "b")))
