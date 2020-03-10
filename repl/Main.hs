8ca2c6cd709da01fb15a7db75b851ba9bb559a3b
Ian Davidson <bontaq@gmail.com>
2020-03-10 00:39:22 -0400

    Add an outline for websocket use
    Copied and thanks to
    https://gist.github.com/andrevdm/9560b5e31933391694811bf22e25c312

--- repl/Main.hs
+++ repl/Main.hs
@@ -1,4 +1,42 @@
+-- {-# LANGUAGE NoImplicitPrelude #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE OverloadedStrings #-}
 module Main where
 
+-- import Protolude
+import qualified Web.Scotty as Sc
+-- import qualified Data.Text as Txt
+import qualified Network.Wai.Middleware.Gzip as Sc
+import qualified Network.Wai.Handler.WebSockets as WaiWs
+import qualified Network.WebSockets as WS
+import qualified Network.Wai as Wai
+import qualified Network.Wai.Handler.Warp as Warp
 
-main = putStrLn "hey"
+main :: IO ()
+main = do
+  let port = 80
+  let settings = Warp.setPort port Warp.defaultSettings
+  sapp <- scottyApp
+  Warp.runSettings settings $ WaiWs.websocketsOr WS.defaultConnectionOptions wsapp sapp
+
+scottyApp :: IO Wai.Application
+scottyApp =
+  Sc.scottyApp $ do
+    Sc.middleware $ Sc.gzip $ Sc.def { Sc.gzipFiles = Sc.GzipCompress }
+    --Sc.middleware S.logStdoutDev
+
+    Sc.get "/" $
+      Sc.file "index.html"
+
+wsapp :: WS.ServerApp
+wsapp pending = do
+  putText "ws connected"
+  conn <- WS.acceptRequest pending
+  WS.forkPingThread conn 30
+
+  (msg :: Text) <- WS.receiveData conn
+  WS.sendTextData conn $ ("initial> " :: Text) <> msg
+
+  forever $ do
+    WS.sendTextData conn $ "loop data"
+    threadDelay $ 1 * 1000000
