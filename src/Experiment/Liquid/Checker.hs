77f13b858469f55bc494d896ffd6e2f84fdd964f
Ian Davidson <ian.davidson@onepeloton.com>
2020-08-05 20:34:46 -0400

    Bloops

--- src/Experiment/Liquid/Checker.hs
+++ src/Experiment/Liquid/Checker.hs
@@ -7,4 +7,7 @@ import Parser
 
 test = syntax $ clex 0 0 "main = 1"
 
+-- since we already have HM inference, just stick with that
+-- we can try out bidirectional checking later
+
 valg = typeCheckCore test []
